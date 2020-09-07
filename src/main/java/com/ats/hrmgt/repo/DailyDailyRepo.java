7ccffa4c362ed3f4011dd28c04863f683cc24bed
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2020-09-07 12:11:18 +0530

    /Akshay

--- src/main/java/com/ats/hrmgt/repo/DailyDailyRepo.java
+++ src/main/java/com/ats/hrmgt/repo/DailyDailyRepo.java
@@ -17,7 +17,8 @@ public interface DailyDailyRepo extends JpaRepository<DailyDaily, Integer> {
 
 	@Query(value = " SELECT id,emp_id,DATE_FORMAT(att_date,'%d-%m-%Y') as att_date, COALESCE( (CONCAT(FLOOR(SUM(ot_hr) / 60), ':',  "
 			+ "	 		 	 LPAD(MOD(SUM(ot_hr),60), 2, '0'))),0) AS ot_hr ,DAYNAME(att_date) as day_name "
-			+ " FROM tbl_attt_daily_daily WHERE att_date BETWEEN :fromDate and :toDate and emp_id=:empId  group by att_date ", nativeQuery = true)
+			+ " FROM tbl_attt_daily_daily WHERE att_date BETWEEN :fromDate and :toDate and emp_id=:empId and is_fixed=0 \n" + 
+			"                and rec_status='O' group by att_date ", nativeQuery = true)
 	List<DailyDaily> getData(@Param("fromDate") String fromDate, @Param("toDate") String toDate,
 			@Param("empId") int empId);
 
