22a507fda6f5af298dae0556d6079481cd4b30f0
lenovo <lenovo@lenovo-Lenovo-ideapad-330-15IKB>
2021-07-08 16:43:43 +0530

    /Akshay

--- src/main/java/com/ats/hrmgt/claim/repository/ClaimReportRepository.java
+++ src/main/java/com/ats/hrmgt/claim/repository/ClaimReportRepository.java
@@ -93,42 +93,7 @@ public interface ClaimReportRepository extends JpaRepository<ClaimReport, Intege
 			"            where\n" + 
 			"                e.emp_id=la.ca_fin_auth_emp_id                                           \n" + 
 			"        ) c                                        \n" + 
-			"            on claim_data.emp_id=c.emp_id    \n" + 
-			"            \n" + 
-			"   SELECT\n" + 
-			"        uuid() as id,claim_data.*,\n" + 
-			"        b.initial_auth_name,\n" + 
-			"        c.final_auth_name,\n" + 
-			"        ifnull(d.emp_remarks,'-') as first_approval_remark,\n" + 
-			"        ifnull(e.emp_remarks,'-') as second_approval_remark\n" + 
-			"    FROM\n" + 
-			"        ( SELECT\n" + 
-			"            la.ca_head_id as claim_id,\n" + 
-			"            la.emp_id, \n" + 
-			"            la.ca_from_dt  ,\n" + 
-			"            la.ca_to_dt , \n" + 
-			"            CONCAT(e.surname,\n" + 
-			"            \" \",\n" + 
-			"            e.first_name) AS emp_name,\n" + 
-			"            la.claim_amount,\n" + 
-			"            la.claim_title,\n" + 
-			"            ct.claim_type_title as claim_type,\n" + 
-			"            ch.claim_amount as detail_amt,\n" + 
-			"            d.name as dept_name\n" + 
-			"        FROM\n" + 
-			"            claim_apply_header la,\n" + 
-			"            m_employees e,\n" + 
-			"            m_department d,\n" + 
-			"            claim_apply ch,\n" + 
-			"            claim_type ct\n" + 
-			"        WHERE\n" + 
-			"            e.emp_id=la.emp_id                       \n" + 
-			"            and la.claim_status = 3 and e.location_id=2 \n" + 
-			"            and date_format(ca_from_dt,'%m') = 06\n" + 
-			"            and date_format(ca_from_dt,'%Y') = 2021\n" + 
-			"            and e.depart_id=d.depart_id \n" + 
-			"            and ch.ex_int2 = la.ca_head_id \n" + 
-			"            and ct.claim_type_id = ch.claim_type_id) claim_data",nativeQuery=true)
+			"            on claim_data.emp_id=c.emp_id ",nativeQuery=true)
 	List<ClaimReport> claimHeaderReport(String month, String year, int locId, List<Integer> deptIds, int sts);
 
 	
